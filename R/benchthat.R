79a2bf0a2c58cc2d31f45522e917b8b885fbad1d
Jim Hester <james.f.hester@gmail.com>
2017-04-24 08:51:51 -0400

    Rough implementation of the idea

--- R/benchthat.R
+++ R/benchthat.R
@@ -1,5 +1,5 @@
 #' @export
-bench_that <- function(code, name = "test", commit = current_commit(), filename = timing_file()) {
+bench_that <- function(code, name = "test", commit = get_commit("HEAD"), filename = timing_file()) {
   time <- tryCatch(system.time(force(code)), error = function(e) {
     message("code exited with error:\n", e$message,
       "\n")
@@ -12,9 +12,13 @@ bench_that <- function(code, name = "test", commit = current_commit(), filename 
 #' @importFrom glue collapse glue
 write_timing <- function(filename, name, commit, time) {
   unless(file.exists(filename), {
-    cat(file = filename, paste0(collapse(c("commit", "benchmark", "user", "system", "total", "child_user", "child_system"), sep = "\t"), "\n"))
+    dir <- dirname(filename)
+    unless(dir.exists(dir), {
+      dir.create(dir, recursive = TRUE)
+    })
+    cat(file = filename, paste0(collapse(c("commit", "commit_date", "benchmark", "user", "system", "total", "child_user", "child_system"), sep = "\t"), "\n"))
   })
-  cat(file = filename, paste0(glue(commit, name, benchmark, collapse(time, sep = "\t"), sep = "\t"), "\n"), append = TRUE)
+  cat(file = filename, paste0(commit@sha, "\t", commit_date(commit), "\t", name, "\t", collapse(time, sep = "\t"), "\n"), append = TRUE)
 }
 
 unless <- function(x, y) {
@@ -43,15 +47,50 @@ bench_prof <- function(code, name = "test", filename = prof_file(), interval = 0
   Rprof(NULL)
 }
 
-current_commit <- function() {
-  tryCatch({
-    r <- git2r::repository(".")
-    h <- git2r::lookup(r, git2r::branch_target(head(r)))
-    h@sha
-  }, error = function(e) "initial")
+get_commit <- function(sha = "HEAD", path = ".") {
+  r <- git2r::repository(project_path(path))
+  if (sha == "HEAD") {
+    ref <- git2r::head(r)
+    if (is(ref, "git_commit")) {
+      return(ref)
+    }
+    git2r::lookup(r, git2r::branch_target(ref))
+  } else {
+    git2r::lookup(r, sha)
+  }
+}
+
+commit_date <- function(commit) {
+  as(commit@author@when, "character")
+}
+
+project_path <- function(path) {
+  rprojroot::find_root(rprojroot::is_git_root, path)
 }
 
 timing_file <- function(path = ".") {
-  root <- rprojroot::find_package_root(path)
+  root <- project_path(path)
   file.path(root, "docs", "benchmarks", "timing.tsv")
 }
+
+process_timings <- function(filename = timing_file()) {
+  library(tidyverse)
+
+  x <- read_tsv(filename, col_types = "ccnnnn")
+
+  x %>%
+    group_by(commit, benchmark) %>%
+    summarise(user = mean(user), system = mean(system), total = mean(total), child_user = mean(child_user), child_system = mean(child_system), n = n())
+  f <- list(x$benchmark, x$commit)
+
+  numeric_means <- function(xx) {
+    is_numeric <- vlapply(xx, is.numeric)
+    xx[is_numeric] <- colMeans(xx[is_numeric])
+    xx
+  }
+  browser()
+
+  unsplit(lapply(split(x, f), numeric_means), f)
+}
+
+vlapply <- function(x, f, ...) vapply(x, f, logical(1), ...)
